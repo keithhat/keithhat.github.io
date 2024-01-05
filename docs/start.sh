ba0023d68c2f6550f02f16d6921be97f9491342f
Keith Hattersley <keith@keith-mbp2.local>
2024-01-06 00:40:43 +1100

    Initial GitHub pages site with Jekyll

---
+++ docs/start.sh
@@ -0,0 +1,9 @@
+# Install specific package version: gem install jekyll -v 3.9.3
+# Ensure have all required dependencies: bundle install
+
+# Can add logging with:
+# bundle exec jekyll serve --trace
+
+bundle exec jekyll serve --livereload
+
+
